cce555caec408ce8c421172f762211eba3fea452
Cristian Yanez <cristian.yanez.canales@gmail.com>
2019-04-29 21:10:26 -0400

    Fixed quotes titles, removed newlines

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,19 +1,19 @@
 export default [
   {
     guid: "a81c10c1-cb61-4910-b65c-8b1892d1e42e",
-    title: "Yuval Noah Harari - Sapiens - Extract on why study history ",
+    title: "Yuval Noah Harari, Sapiens",
     text:
       "So why study history? Unlike physics or economics, history is not a means for making accurate predictions. We study history not to know the future but to widen our horizons, to understand that our present situation is neither natural nor inevitable, and that we consequently have many more possibilities before us than we imagine."
   },
   {
     guid: "9f5fb327-e24a-4a60-81db-39487b0d2522",
-    title: "Yuval Noah Harari - Sapiens - Extract on human (non) dominance",
+    title: "Yuval Noah Harari, Sapiens",
     text:
       "We assume that a large brain, the use of tools, superior learning abilities and complex social structures are huge advantages. It seems self-evident that these have made humankind the most powerful animal on earth. But humans enjoyed all of these advantages for a full 2 million years during which they remained weak and marginal creatures."
   },
   {
     guid: "21aeaa48-c9ec-4877-8cd2-c8a3a0adc5c2",
-    title: "Yuval Noah Harari - Sapiens - Extract on tools and evolution",
+    title: "Yuval Noah Harari, Sapiens",
     text:
       "For more than 2 million years, human neural networks kept growing and growing, but apart from some flint knives and pointed sticks, humans had precious little to show for it. What then drove forward the evolution of the massive human brain during those 2 million years? Frankly, we don't know."
   },
@@ -27,6 +27,6 @@ export default [
     guid: "41612de7-1fbb-4472-bc6a-3ae848fe93b4",
     title: "Douglas Adams, The Hitchhiker's Guide to the Galaxy",
     text:
-      '"You know" said Arthur, "it\'s at times like this, when I\'m trapped in a Vogon airlock with a man from Betelgeuse, and about to die of asphyxiation in deep space that I really wish I\'d listened to what my mother told me when I was young."\n"Why, what did she tell you?"\n"I don\'t know, I didn\'t listen."'
+      '"You know" said Arthur, "it\'s at times like this, when I\'m trapped in a Vogon airlock with a man from Betelgeuse, and about to die of asphyxiation in deep space that I really wish I\'d listened to what my mother told me when I was young." "Why, what did she tell you?" "I don\'t know, I didn\'t listen."'
   }
 ];
