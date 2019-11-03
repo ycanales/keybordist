6aec1c7cfa3341c365244b0a831dbfc219b5217d
Cristian Yanez <cristian.yanez.canales@gmail.com>
2019-11-03 14:40:10 -0300

    adds sort by date to MyScores

--- src/MyScores.js
+++ src/MyScores.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import StyledForm from "./StyledForm";
 import { getDisplayText } from "./MySetups";
 import { UncommonSpan } from "./nord";
 
+const DATE_ASCENDING = "+date";
+const DATE_DESCENDING = "-date";
+
 const MyScores = ({ data, setups, ...rest }) => {
   if (data && data.length) {
+    const [sortedData, setSortedData] = useState([...data]);
+    const [currentSort, setCurrentSort] = useState(DATE_DESCENDING);
+
+    const sortByDate = () => {
+      if (currentSort === DATE_DESCENDING) {
+        setSortedData(
+          [...sortedData].sort((a, b) => (a.date < b.date ? -1 : 1))
+        );
+        setCurrentSort(DATE_ASCENDING);
+      } else {
+        setSortedData(
+          [...sortedData].sort((a, b) => (a.date > b.date ? -1 : 1))
+        );
+        setCurrentSort(DATE_DESCENDING);
+      }
+    };
+
     return (
       <StyledForm>
-        {data.length === 1 ? (
+        {sortedData.length === 1 ? (
           <h2 className="message">You have typed one quote.</h2>
         ) : (
           <h2 className="message">You have played {data.length} times.</h2>
@@ -21,14 +41,16 @@ const MyScores = ({ data, setups, ...rest }) => {
         <table>
           <thead>
             <tr>
-              <th>Date</th>
+              <th>
+                <button onClick={sortByDate}>Date</button>
+              </th>
               <th>Setup</th>
               <th>WPM</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {data.map(score => (
+            {sortedData.map(score => (
               <tr key={score.uuid}>
                 <td>{new Date(score.date).toLocaleDateString()}</td>
                 <td>
